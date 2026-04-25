974672ff5c1f147da02ce26fa951a476d5f1d42c
Huyen Dinh <huyen061610@gmail.com>
2026-04-25 23:22:22 +0300

    Update build_push_app

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM python:3.10-slim
-
+    
 WORKDIR /app 
 
 COPY app/requirements.txt .
 RUN pip install -r requirements.txt 
 
 COPY app/ .
+
+COPY registered_model ./registered_model
 
 EXPOSE 8000
 CMD [ "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000" ]
--- app/main.py
+++ app/main.py
@@ -5,23 +5,25 @@ import io
 import os 
 
 from azure.ai.ml import MLClient
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, AzureCliCredential
 
 app = FastAPI(
     title="Chicago Ticket Payment Prediction"
 ) 
 # load local
-# model = mlflow.sklearn.load_model("registered_model")
+model = mlflow.sklearn.load_model("registered_model")
 
 # load from aml registry 
-mlclient = MLClient(
-    DefaultAzureCredential(),
-    subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
-    resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
-    workspace_name=os.environ["AZURE_WORKSPACE_NAME"]
-) 
-mlflow.set_tracking_uri(mlclient.workspaces.get(mlclient.workspace_name).mlflow_tracking_uri)
-model = mlflow.sklearn.load_model(model_uri="models:/ChicagoParkingTickets_model/latest")
+# mlclient = MLClient(
+#     DefaultAzureCredential(),
+#     subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
+#     resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
+#     workspace_name=os.environ["AZURE_WORKSPACE_NAME"]
+# ) 
+# mlflow.set_tracking_uri(mlclient.workspaces.get(mlclient.workspace_name).mlflow_tracking_uri)
+# model = mlflow.sklearn.load_model(model_uri="models:/ChicagoParkingTickets_model/latest")
+
+
 
 @app.get("/")
 def root():
