9b1ce7eef21d4584d49fb1983078c408cc08b66a
Huyen Dinh <huyen061610@gmail.com>
2026-04-26 09:56:15 +0300

    Update sample.csv path

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -13,8 +13,9 @@ def test_predict_wrong_format():
     response = client.post("/predict", files={"file": ("text.txt", b"data", "text/plain")}) # create a txt file
     assert response.status_code == 400 
 
-def test_predict_csv(): 
-    with open("sample.csv", "rb") as f: 
+def test_predict_csv():
+    path = Path(__file__).parent / "sample.csv"
+    with open(path, "rb") as f:
         response = client.post("/predict", files={"file": f})
     assert response.status_code == 200 
     assert "predictions" in response.json()
