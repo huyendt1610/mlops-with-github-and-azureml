3cd7dc68d74bfc5fcf54ef8e26b6f508e0d79292
Huyen Dinh <huyen061610@gmail.com>
2026-04-27 02:46:36 +0300

    Fix Testing

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -2,11 +2,11 @@ from fastapi.testclient import TestClient
 from unittest.mock import patch, MagicMock
 from pathlib import Path
 
-mock_model = MagicMock() 
+mock_model = MagicMock()
 mock_model.predict.return_value = [1, 0]
-mock_model.predict_proba.return_value = [[0.3, 0.7],[0.8, 0.2]]
+mock_model.predict_proba.return_value = [[0.3, 0.7], [0.8, 0.2]]
 
-with patch("model.MLClient"), patch("model.mlflow", return_value=mock_model):
+with patch("model.load_model", return_value=mock_model):
     from main import app
 
 client = TestClient(app)
